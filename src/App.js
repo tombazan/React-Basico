051718136ccbf73558a6f07a810d7f744b5876d2
tombazan <tomasbazan95@gmail.com>
2021-03-09 20:09:47 -0300

    Creando component de Producto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Producto from './components/Producto';
 
 function App() {
+
+  //Crear listado de productos
+  const [ productos, guardarProductos ] = useState([
+    { id: 1, nombre: 'Camisa ReactJS', precio: 50},
+    { id: 2, nombre: 'Camisa VueJS', precio: 55},
+    { id: 3, nombre: 'Camisa NodeJS', precio: 50},
+    { id: 4, nombre: 'Camisa ReactJS', precio: 60},
+    { id: 5, nombre: 'Camisa Angular', precio: 60},
+  ]);
 
   //Obtener la fecha
   const fecha = new Date().getFullYear();
@@ -13,6 +23,13 @@ function App() {
         titulo= 'Tienda Virtual'
       />
 
+      <h1>Lista de Productos</h1>
+      {productos.map(producto => (
+        <Producto 
+          key={producto.id}
+          producto={producto}
+        />
+      ))}
       <Footer
         fecha= {fecha}
       />
